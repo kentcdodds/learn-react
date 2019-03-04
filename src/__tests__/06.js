e677f229aa0725b96745f6d10310043e744f2e5a
Kent C. Dodds <kent@doddsfamily.us>
2019-03-04 08:20:03 -0700

    add useful comment

--- src/__tests__/06.js
+++ src/__tests__/06.js
@@ -24,6 +24,7 @@ test('Usage works', async () => {
   expect(button).toHaveTextContent(/4/)
   fireEvent.click(button)
   expect(button).toHaveTextContent(/5/)
+  // make sure that localStorage.getItem is only called once despite multiple re-renders
   expect(Storage.prototype.getItem).toHaveBeenCalledTimes(1)
   expect(window.localStorage.getItem('count')).toBe('5')
 })
