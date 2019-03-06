d2c0dba5f9c6c112c5c0baf1eff3d1c698b7d33f
Kent C. Dodds <kent@doddsfamily.us>
2019-03-05 23:07:02 -0700

    finish 16

--- src/exercises/16.js
+++ src/exercises/16.js
@@ -1,30 +1,64 @@
 // Making HTTP requests
 import React from 'react'
 
-// In this exercise we have a form where users can enter the name of a pokemon
-// and fetch data about that pokemon. Your job will be to create a component
-// which makes that fetch request.
+// 🦉 There are lots of ways to make HTTP requests in React components. React is
+// currently working on a new feature called Suspense which is fantastic and
+// will solve lots of problems web applications (not just React) have with
+// data fetching on the client. Unfortunately, it's not ready yet.
 //
-// 🐨 Have state for the pokemon and the loading state
-// 🐨 Use the `fetchPokemon` function below to fetch a pokemon by its name:
-//   fetchPokemon('Pikachu').then(
-//     pokemon => { /* call set state with the pokemon and loading: false */},
-//     error => {/* call set state with the error loading: false */},
-//   )
-// 🐨 You should fetch the pokemon in componentDidMount and componentDidUpdate
-//    (but only if the props changed: `componentDidMount(prevProps)`)
-// 🐨 Before you fetch the pokemon, call `this.setState({loading: true})` to
-//   initialize the loading state (ask me later how this will be different in
-//   the future with React "suspense")
-// 🐨 Render the appropriate content based on the state:
-//    1. loading: '...'
-//    2. error: 'ERROR!'
-//    3. pokemon: the JSON.stringified pokemon in a <pre></pre>
+// 🦉 Every company has unique data requirements, so I recommend that you either
+// find an open source custom hook that suits your needs or you develop one
+// for your company that suits your needs well.
+//
+// In this exercise, we'll be doing data fetching directly in a useEffect hook
+// callback within our component.
+//
+// Here we have a form where users can enter the name of a pokemon and fetch
+// data about that pokemon. Your job will be to create a component which makes
+// that fetch request.
 
-class FetchPokemon extends React.Component {
-  render() {
-    return 'todo...'
+// eslint-disable-next-line no-unused-vars
+function fetchPokemonReducer(state, action) {
+  switch (action.type) {
+    case 'FETCHING': {
+      // 🐨 return the state that should exist when fetching starts
+      return state
+    }
+    case 'FETCHED': {
+      // 🐨 return the state that should exist when the fetch request finishes
+      return state
+    }
+    case 'FETCH_ERROR': {
+      // 🐨 return the state that should exist when the fetch request fails
+      return state
+    }
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`)
   }
+}
+
+function FetchPokemon({pokemonName}) {
+  // 🐨 Have state for the pokemon (null), the error state (null), and the
+  // loading state (false). I recommend you use a reducer for this. I've given
+  // you a starter reducer above because I love you.
+  // 🐨 Use the `fetchPokemon` function below to fetch a pokemon by its name:
+  //   fetchPokemon('Pikachu').then(
+  //     pokemon => { /* call set state with the pokemon and loading: false */},
+  //     error => {/* call set state with the error loading: false */},
+  //   )
+
+  // 🐨 use React.useEffect where the callback should be called whenever the
+  // pokemon name changes.
+  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
+  // 🐨 before calling `fetchPokemon`, make sure to dispatch a FETCHING action
+  // 🐨 when the promise resolves, dispatch FETCHED and send the pokemon
+  // 🐨 if the promise rejects, dispatch a FETCH_ERROR and send the error
+
+  // 🐨 Render the appropriate content based on the state:
+  //    1. loading: '...'
+  //    2. error: 'ERROR!'
+  //    3. pokemon: the JSON.stringified pokemon in a <pre></pre>
+  return 'todo'
 }
 
 /////////////////////////////////////////////////
